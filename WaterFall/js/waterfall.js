58d5bf8d83a1805ae33fc0739036bed0da458570
毛蓓君 <maobeijun@ukitchenplus.com>
2016-06-15 17:16:45 +0800

    water fall init commit

---
+++ WaterFall/js/waterfall.js
@@ -0,0 +1,88 @@
+/***
+    waterfall.js https://github.com/queentvxq
+****/    
+    var WaterFall = function (param){
+        this.id = typeof param.container == 'string' ? document.getElementById(param.container) : param.container;
+        this.colWidth = param.colWidth;
+        this.colWidthUnit = param.colWidthUnit||'px';
+        this.colCount = param.colCount || 2;
+        this.cls = param.cls && param.cls != '' ? param.cls : 'wf-cld';
+        this.init();
+    }
+    /***
+    ** config with '*' is necessary
+    ****/
+    WaterFall.prototype = {
+        id: '',//* container ID
+        colWidth: '',//* column width
+        colWidthUnit: 'px',//unit of column width,default is 'px'
+        colCount: 2,//the number of columns,default is 2
+        cls: 'wf-cld',//className of child elements,default is 'wf-cld'
+        getByClass:function(cls,p){
+            var arr = [],reg = new RegExp("(^|\\s+)" + cls + "(\\s+|$)","g");
+            var nodes = p.getElementsByTagName("*"),len = nodes.length;
+            for(var i = 0; i < len; i++){
+                if(reg.test(nodes[i].className)){
+                    arr.push(nodes[i]);
+                    reg.lastIndex = 0;
+                }
+            }
+            return arr;
+        },
+        maxArr:function(arr){
+            var len = arr.length,temp = arr[0];
+            for(var ii= 1; ii < len; ii++){
+                if(temp < arr[ii]){
+                    temp = arr[ii];
+                }
+            }
+            return temp;
+        },
+        getMar:function(node){
+            var dis = 0;
+            if(node.currentStyle){
+                dis = parseInt(node.currentStyle.marginBottom);
+            }else if(document.defaultView){
+                dis = parseInt(document.defaultView.getComputedStyle(node,null).marginBottom);
+            }
+            return dis;
+        },
+        getMinCol:function(arr){
+            var ca = arr,cl = ca.length,temp = ca[0],minc = 0;
+            for(var ci = 0; ci < cl; ci++){
+                if(temp > ca[ci]){
+                    temp = ca[ci];
+                    minc = ci;
+                }
+            }
+            return minc;
+        },
+        init:function(){
+            var _this = this;
+            var col = [],//column height
+                iArr = [];//index
+            var nodes = _this.getByClass(_this.cls,_this.id),len = nodes.length;
+            for(var i = 0; i < _this.colCount; i++){
+                col[i] = 0;
+            }
+            for(var i = 0; i < len; i++){
+                nodes[i].h = nodes[i].offsetHeight + _this.getMar(nodes[i]);
+                iArr[i] = i;
+            }
+            
+            for(var i = 0; i < len; i++){
+                var ming = _this.getMinCol(col);
+                nodes[i].style.left = ming * _this.colWidth + _this.colWidthUnit;
+                nodes[i].style.top = col[ming] + "px";
+                col[ming] += nodes[i].h;
+            }
+            
+            _this.id.style.height = _this.maxArr(col) + "px";
+        }
+    };
+
+    var myWaterFall = new WaterFall({
+        "container":"wf-main",
+        "colWidth":'47',
+        "colWidthUnit":'%',
+    });
